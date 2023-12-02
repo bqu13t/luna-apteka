3cacda606a2676ff421d6dca6e69ddd742d28e20
bqu13t <bqu13t@proton.me>
2023-12-02 17:08:20 +0300

    added not-found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,17 @@
+import Link from 'next/link'
+import Header from '@/components/header/header'
+import Footer from '@/components/footer/footer'
+
+export default function NotFound() {
+  return (
+    <>
+      <Header />
+      <main className='not-found-page'>
+        <h2>Ошибка 404. Запрашиваемая страница не найдена</h2>
+        <p>Похоже такой странички не существует</p>
+        <Link href="/">Вернуться на главную страницу</Link>
+      </main>
+      <Footer />
+    </>
+  )
+}
