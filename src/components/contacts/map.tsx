b6c7a3e7b585ee4046adba9c1fd7026be7032318
bqu13t <bqu13t@proton.me>
2023-12-03 16:16:28 +0300

    changed flyTo function

--- src/components/contacts/map.tsx
+++ src/components/contacts/map.tsx
@@ -4,7 +4,7 @@ import React, { useRef, useEffect, useState } from 'react';
 import mapboxgl, { Map as CustomMap } from 'mapbox-gl';
 import "mapbox-gl/dist/mapbox-gl.css";
 
-mapboxgl.accessToken = 'pk.eyJ1IjoiYnF1MTN0IiwiYSI6ImNscGlsYzNzajAwbnUyaW83bzdyZmFieG8ifQ.NieytJ-XGbivXz7nXv3sNw';
+mapboxgl.accessToken = 'pk.eyJ1IjoiYnF1MTN0IiwiYSI6ImNscGlsYzNzajAwbnUyaW83bzdyZmFieG8ifQ.NieytJ-XGbivXz7nXv3sNw'
 
 type ExtendedMap = CustomMap & {
   setConfigProperty: (layer: string, property: string, value: string | boolean) => void;
@@ -14,17 +14,16 @@ export default function Map() {
   const mapContainer = useRef<HTMLDivElement | null>(null);
   const map = useRef<mapboxgl.Map | null>(null);
   const [lng, setLng] = useState(82.90314555499413);
-  const [lat, setLat] = useState(90);
-  const [zoom, setZoom] = useState(3);
-  const [pitch, setPitch] = useState(40);
+  const [lat, setLat] = useState(55.06584507569244);
+  const [zoom, setZoom] = useState(15.3);
+  const [pitch, setPitch] = useState(45);
   const [offset, setOffset] = useState(0);
 
   useEffect(() => {
     if (map.current) return; // initialize map only once
 
-
     const colorScheme = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
-    const lightPreset = colorScheme === 'dark' ? 'dusk' : 'dawn';
+    const lightPreset = colorScheme === 'dark' ? 'night' : 'dawn';
 
     map.current = new mapboxgl.Map({
       container: mapContainer.current!,
@@ -47,14 +46,13 @@ export default function Map() {
     });
 
     const screenWidth = window.innerWidth
-    console.log(screenWidth)
     const screenHeight = window.innerHeight
 
     const currentOffset = ({ screenWidth, screenHeight }: { screenWidth: number, screenHeight: number }): mapboxgl.PointLike => {
       if (screenWidth < 640) {
         return [0, screenHeight - (screenHeight * 0.80)];
       } else {
-        return [0, 0]; // Возвращаем нулевой сдвиг для больших экранов
+        return [0, 0];
       }
     }
 
@@ -63,7 +61,7 @@ export default function Map() {
       map.current?.flyTo({
         center: [82.90303531983233, 55.06584507569244],
         essential: true,
-        zoom: 16.2,
+        zoom: 16.5,
         duration: 15000,
         offset: offset
       });
@@ -72,8 +70,6 @@ export default function Map() {
     map.current.on('style.load', () => {
       fly();
     });
-
-    map.current?.scrollZoom.enable();
   }, [lng, lat, zoom, pitch]);
 
   return (
