a4821b5d9a8ebf2565047c745e2b1e52a6bb7de4
io <io@ios.local>
2025-02-20 15:10:59 +0300

    made product features visible

--- src/components/main/product-card/product-card.tsx
+++ src/components/main/product-card/product-card.tsx
@@ -123,7 +123,7 @@ export default function ProductCard({ id }: { id: string }) {
                   <p className="value"><strong>{weightSm} гр</strong></p>
                 </div>
               </div>
-              {/* <div
+              <div
                 className={clsx("product-feature", {
                   "product-feature-hidden": isMeatless === true,
                 })}
@@ -135,8 +135,8 @@ export default function ProductCard({ id }: { id: string }) {
                   <span className="title">Мясная начинка</span>
                   <p className="value"><strong>{Math.round(meatWeightSm! / weightSm! * 100)}% - {meatWeightSm} гр</strong></p>
                 </div>
-              </div> */}
-              {/* <div className="product-feature">
+              </div>
+              <div className="product-feature">
                 <div className="product-feature__icon">
                   <CheeseIcon />
                 </div>
@@ -144,7 +144,7 @@ export default function ProductCard({ id }: { id: string }) {
                   <span className="title">Cырная начинка</span>
                   <p className="value"><strong>{Math.round(cheeseWeightSm! / weightSm! * 100)}% - {cheeseWeightSm} гр</strong></p>
                 </div>
-              </div> */}
+              </div>
               <div className="product-feature">
                 <div className="product-feature__icon">
                   <PriceIcon />
@@ -192,7 +192,7 @@ export default function ProductCard({ id }: { id: string }) {
                   <p className="value"><strong>{weightLg} гр</strong></p>
                 </div>
               </div>
-              {/* <div
+              <div
                 className={clsx("product-feature", {
                   "product-feature-hidden": isMeatless === true,
                 })}
@@ -204,8 +204,8 @@ export default function ProductCard({ id }: { id: string }) {
                   <span className="title">Мясная начинка</span>
                   <p className="value"><strong>{Math.round(meatWeightLg! / weightLg! * 100)}% - {meatWeightLg} гр</strong></p>
                 </div>
-              </div> */}
-              {/* <div className="product-feature">
+              </div>
+              <div className="product-feature">
                 <div className="product-feature__icon">
                   <CheeseIcon />
                 </div>
@@ -213,7 +213,7 @@ export default function ProductCard({ id }: { id: string }) {
                   <span className="title">Cырная начинка</span>
                   <p className="value"><strong>{Math.round(cheeseWeightLg! / weightLg! * 100)}% - {cheeseWeightLg} гр</strong></p>
                 </div>
-              </div> */}
+              </div>
               <div className="product-feature">
                 <div className="product-feature__icon">
                   <PriceIcon />
